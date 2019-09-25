92173c9c4ed035043ef739c1918d8f64d6b8d668
dky <103966660@qq.com>
2019-09-25 19:06:07 +0800

    push finish

--- app.js
+++ app.js
@@ -33,9 +33,41 @@ App({
       }
     })
 
+    function parseTime(time, fmt) {
+      if (arguments.length === 0) {
+        return null
+      }
+      const format = fmt || '{y}-{m}-{d} {h}:{i}:{s}'
+      let date
+      if (typeof time === 'object') {
+        date = time
+      } else {
+        if (('' + time).length === 10) time = parseInt(time) * 1000
+        date = new Date(time)
+      }
+      const formatObj = {
+        y: date.getFullYear(),
+        m: date.getMonth() + 1,
+        d: date.getDate(),
+        h: date.getHours(),
+        i: date.getMinutes(),
+        s: date.getSeconds(),
+        a: date.getDay()
+      }
+      const timeStr = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+        let value = formatObj[key]
+        if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
+        if (result.length > 0 && value < 10) {
+          value = '0' + value
+        }
+        return value || 0
+      })
+      return timeStr
+    }
     var taskSocket = wx.connectSocket({
       // url: 'wss://websck.eloeg.wang:20001',
-      url: 'ws://10.10.92.161:8901/websocket/doufu',
+      // url: 'wss://aliiot.on-bright.com/webWxSocket/adolf',
+      url: 'ws://192.168.200.116:8901/webWxSocket/fasfsafas22'
       // url: 'wss://aliiot.on-bright.com/queueServer',
     })
 
@@ -46,28 +78,35 @@ App({
       console.log('onSocketOpen')
     })
 
-    taskSocket.onMessage((data) => {
+    taskSocket.onMessage(({
+      data
+    }) => {
       console.log('onMessage')
-      console.log(data)
-      wx.showModal({
-        content: '收到呼叫',
-        showCancel: true,
-        cancelText: '忽略',
-        confirmText: '前往',
-        success: function(res) {
-          if (res.confirm) {
-            console.log('用户点击确定')
-          } else if (res.cancel) {
-            console.log('用户点击取消')
-          }
-        },
-        fail: function(res) {
-          console.log('fail')
-        },
-        complete: function(res) {
-          console.log('complete')
-        },
-      })
+      var jo = JSON.parse(data)
+      var show = (jo.type == 1)
+      console.log(jo);
+      if (show) {
+        var time = parseTime(jo.alarmTime*1)
+        wx.showModal({
+          content: time+'曾锦秀SSO呼叫\n',
+          showCancel: true,
+          cancelText: '忽略',
+          confirmText: '前往',
+          success: function(res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          },
+          fail: function(res) {
+            console.log('fail')
+          },
+          complete: function(res) {
+            console.log('complete')
+          },
+        })
+      }
     })
   },
   globalData: {
